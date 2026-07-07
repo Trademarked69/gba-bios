63083151fc5d5d08311af1ec3c2ba67e29f2e1db
Trademarked69 <193121288+Trademarked69@users.noreply.github.com>
2026-07-07 09:02:52 -0500

    Anbernic Logo

--- source/logo_data.h
+++ source/logo_data.h
@@ -5,11 +5,11 @@
 //
 //	logo_data, 256x256@4, 
 //	+ palette 16 entries, not compressed
-//	+ 141 tiles (t|f reduced) lz77 compressed
+//	+ 48 tiles (t|f reduced) lz77 compressed
 //	+ regular map (in SBBs), lz77 compressed, 32x32 
-//	Total size: 32 + 2812 + 536 = 3380
+//	Total size: 32 + 928 + 348 = 1308
 //
-//	Time-stamp: 2026-01-19, 16:28:58
+//	Time-stamp: 2026-07-03, 08:10:13
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,11 +18,11 @@
 #ifndef GRIT_LOGO_DATA_H
 #define GRIT_LOGO_DATA_H
 
-#define logo_dataTilesLen 2812
-extern const unsigned int logo_dataTiles[703];
+#define logo_dataTilesLen 928
+extern const unsigned int logo_dataTiles[232];
 
-#define logo_dataMapLen 536
-extern const unsigned short logo_dataMap[268];
+#define logo_dataMapLen 348
+extern const unsigned short logo_dataMap[174];
 
 #define logo_dataPalLen 32
 extern const unsigned short logo_dataPal[16];
